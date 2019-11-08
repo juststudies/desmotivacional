1458ce80a3fb817e3959c8b830d76d37facd5710
Nicolas Codonho <35177809+juststudies@users.noreply.github.com>
2019-11-08 17:12:50 -0300

    desespero

---
+++ build/precache-manifest.a12b48065c5ac2719b5f8f7f5f576385.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac82c124bb47ab6204928316557ef1d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00bd81444f7a06fc6f72",
+    "url": "/static/css/main.16919e4c.chunk.css"
+  },
+  {
+    "revision": "169707a9e3cead32323f",
+    "url": "/static/js/2.4507439f.chunk.js"
+  },
+  {
+    "revision": "00bd81444f7a06fc6f72",
+    "url": "/static/js/main.12e8fb7f.chunk.js"
+  },
+  {
+    "revision": "aa5dd861c3df341bfb4f",
+    "url": "/static/js/runtime-main.0fda84ed.js"
+  }
+]);
